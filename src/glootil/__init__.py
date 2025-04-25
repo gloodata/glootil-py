05cf5a2864462b4713013ef77c5b8c832d9c6ee6
Mariano Guerra <mariano@marianoguerra.org>
2025-04-25 09:52:54 +0200

    force dyn enum str representation to be a string

--- src/glootil/__init__.py
+++ src/glootil/__init__.py
@@ -596,7 +596,7 @@ class DynEnum:
         return f"DynEnum({self.name}, {self.value})"
 
     def __str__(self):
-        return self.name
+        return str(self.name)
 
     def __eq__(self, other):
         return (
